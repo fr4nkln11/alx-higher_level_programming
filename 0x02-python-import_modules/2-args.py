9110501cc471467aa13ee8a3a45b047e76b5c9ca
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-10-05 13:38:05 +0100

    2-args.py2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,12 @@ import sys
 argv = sys.argv
 argc = len(argv) - 1
 
-print("{} argument".format(argc), end="")
-if argc != 1:
-    print("s", end="")
-print(".") if argc == 0 else print(":")
+if __name__ == "__main__":
+    print("{} argument".format(argc), end="")
+    if argc != 1:
+        print("s", end="")
+    print(".") if argc == 0 else print(":")
 
-if argc >= 1:
-    for i in range(1, argc + 1):
-        print("{}: {}".format(i, argv[i]))
+    if argc >= 1:
+        for i in range(1, argc + 1):
+            print("{}: {}".format(i, argv[i]))
