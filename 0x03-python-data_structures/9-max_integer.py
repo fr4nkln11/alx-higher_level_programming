bfc9fe057199af27dd6e458c2634502d63c66863
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-10-07 18:27:05 +0100

    0x03 [Task 9] correction 1

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_int = 0
-    
     if my_list:
+        max_int = my_list[0]
         for num in my_list:
             if num > max_int:
                 max_int = num
