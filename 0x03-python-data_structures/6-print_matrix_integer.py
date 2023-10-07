813be98edb2d2b2106f12dca163ba60c037d1d6b
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-10-07 11:31:23 +0100

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,9 +3,10 @@ def print_matrix_integer(matrix=[[]]):
     if matrix:
         inner_length = len(matrix[0])
         for inner in matrix:
-            for i in range(inner_length):
-                if i < inner_length - 1:
-                    print("{:d} ".format(inner[i]), end="")
-                else:
-                    print("{:d}".format(inner[i]))
+            if inner:
+                for i in range(inner_length):
+                    if i < inner_length - 1:
+                        print("{:d} ".format(inner[i]), end="")
+                    else:
+                        print("{:d}".format(inner[i]))
 
