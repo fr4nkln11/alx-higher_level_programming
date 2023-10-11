60c644a81669c4960832ddf7f0e3143aae62613c
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-10-11 23:48:53 +0100

    0-square_matrix_simple.py1

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    new_matrix = []
     if matrix:
-        new_matrix = [list(map(lambda e: e ** 2 ,x)) for x in matrix]
-        return new_matrix
-    else:
-        return []
+        for vec in matrix:
+            n_vec = [el for el in vec]
+            new_matrix.append(list(map(lambda e: e ** 2, n_vec)))
+
+    return new_matrix
