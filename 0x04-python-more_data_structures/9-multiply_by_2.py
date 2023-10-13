e1477030f6713e8497da8fbefed0417333c07971
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-10-13 10:09:03 +0100

    9-multiply_by_2.py

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dict = {}
+    if a_dictionary:
+        new_dict = {name:num * 2 for name, num in a_dictionary.items()}
+
+    return new_dict
