e301ed4a2cd14846990461bb14a4eb64c2ad8eb2
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-10-23 17:02:30 +0100

    [Task 2]

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    counter = 0
+    for i in range(x):
+        try:
+            el = my_list[i]
+            print("{:d}".format(el), end="")
+            counter += 1
+        except (TypeError, ValueError):
+            pass
+        except IndexError:
+            raise IndexError
+
+    print("")
+    return counter
