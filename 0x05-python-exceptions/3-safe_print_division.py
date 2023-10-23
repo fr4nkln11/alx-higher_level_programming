761b136894efca392811bd4165235f0728552410
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-10-23 17:26:43 +0100

    [Task 3]

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def safe_print_division(a: int, b: int) -> float | None:
+    result = 0
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("{}".format(result))
+        return result
+
