48632a8a5d8e0e2463394db138528899e1036eff
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-10-23 10:00:34 +0100

    [task 1]

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    counter = 0
+    for i in range(x):
+        try:
+            el = my_list[i]
+            print("{}".format(el), end="")
+            counter += 1
+        except IndexError:
+            break
+
+    print("")
+    return counter
