c071af4cae90382a92135bdadbee3db6864cd18c
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-10-24 19:13:44 +0100

    [task 1]

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+""" 1-Square
+This module defines
+a class Square that defines a square by: (based on 0-square.py)
+
+Private instance attribute: size
+Instantiation with size (no type/value verification)
+"""
+
+
+class Square:
+    """ Square Class
+    a class Square that defines a square by: (based on 0-square.py)
+    Private instance attribute: size
+    Instantiation with size (no type/value verification)
+    """
+    def __init__(self, size):
+        self.__size = size
