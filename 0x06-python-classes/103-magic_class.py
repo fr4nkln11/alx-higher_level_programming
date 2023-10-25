2249c782a73f38c8054300a29a271b0b4f355b93
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-10-25 22:37:03 +0100

    [Task 10]

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
+""" Bytecode -> Python #5
+This module defines a MagicClass
+"""
 import math
 
 
 class MagicClass:
+    """ a MagicClass defined by:
+        Public instance method: def area(self)
+        Public instance method: def circumference(self)
+
+        Instantiation with radius:
+            def __init__(self, radius):
+    """
     def __init__(self, radius):
         if type(radius) is not int:
             if type(radius) is not float:
@@ -11,7 +21,13 @@ class MagicClass:
         self.__radius = radius
 
     def area(self):
+        """ calculate the area
+        with self.radius and PI
+        """
         return ((self.__radius ** 2) * math.pi)
 
     def circumference(self):
+        """ calculate the circumference
+        with self.radius and PI
+        """
         return (self.__radius * (2 * math.pi))
