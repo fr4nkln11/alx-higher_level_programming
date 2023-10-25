6c3f2553133c6bb4efab83e49ec193a70006c221
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-10-25 09:50:12 +0100

    [Task 7]

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,5 +6,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except (ValueError, TypeError) as e:
-        stderr.write("Exception: {}".format(e))
+        stderr.write("Exception: {}\n".format(e))
         return False
