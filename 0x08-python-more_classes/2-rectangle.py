c92094b7f1024c411f0a0c52b65f32d6e51ea9b6
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-10-30 20:12:28 +0100

    [Task 2] correction 1

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -98,4 +98,6 @@ class Rectangle:
         Return:
             Perimeter of Rectangle
         """
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return (2 * self.__width) + (2 * self.__height)
