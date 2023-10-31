6bec9a7c8c053ae0c535be275639e23deab56427
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-10-31 21:52:04 +0100

    [TDD]

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" Say my name
+This module defines a function say_my_name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    if not type(first_name) is str:
+        raise TypeError("first_name must be a string")
+
+    if not type(last_name) is str:
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
