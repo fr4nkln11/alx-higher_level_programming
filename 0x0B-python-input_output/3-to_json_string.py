06dcfd063f8d19f2d15efd348b6d4a22c1889d8a
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-12-05 23:32:15 +0100

    to json string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+import json
+
+"""
+This module defines:
+
+functions:
+to_json_string
+"""
+
+
+def to_json_string(my_obj):
+    """
+    a function that returns the JSON representation of an object (string)
+
+    params:
+    @my_obj: object to serialize
+
+    returns:
+    serialize data in string
+    """
+
+    return json.dumps(my_obj)
+
