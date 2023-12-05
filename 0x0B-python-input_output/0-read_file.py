40cda4a32bea05e89ea7fdff856ebc046cd59c3d
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-12-05 21:17:43 +0100

    add docstrings to read_file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,5 +8,15 @@ read_file
 
 
 def read_file(filename=""):
+    """
+    a function that reads a text file (UTF8)
+    and prints it to stdout
+
+    params:
+    @filename: path to the file
+
+    returns:
+    Nothing
+    """
     with open(filename, "r", encoding="utf-8") as file:
         print(file.read())
