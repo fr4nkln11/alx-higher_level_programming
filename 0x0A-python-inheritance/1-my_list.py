f2d26b76e3093eb577c31c9fe2cf864108f84981
Franklin Ikeh <ikehfranklind3c0d3r@gmail.com>
2023-12-07 14:51:17 +0100

    implement MyList

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+This module defines:
+
+class:
+MyList
+"""
+
+
+class MyList(list):
+    """
+    a class MyList that inherits from list
+    Public instance method:
+        def print_sorted(self):
+            prints the list, but sorted (ascending sort)
+    """
+
+    def print_sorted(self):
+        print(sorted(self))
